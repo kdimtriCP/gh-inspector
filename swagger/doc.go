93cef06eafc09e113082032f9436899bfc5279c5
kdimtri <kurunov.dmitry@gmail.com>
2025-07-31 20:03:25 +0200

    api

---
+++ swagger/doc.go
@@ -0,0 +1,7 @@
+//go:build !noswagger
+// +build !noswagger
+
+// Package swagger contains generated API documentation.
+// This package is imported for its side effects - it registers
+// the swagger documentation on import.
+package swagger
